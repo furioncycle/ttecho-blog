40be42f578ba20ea73cd71e639ab90b9c42091d7
ttecho <ttecho2021@gmail.com>
2023-01-08 17:39:35 -0800

    First Header trial - example from SPA

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,34 @@
 module Pages.Home_ exposing (page)
 
-import Html
+import Html exposing (..)
+import Html.Attributes as Attr exposing (class)
 import View exposing (View)
 
 
 page : View msg
 page =
-    { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    { title = "Wondering & Tinkering"
+    , body =
+        [ viewHeader
+        ]
     }
+
+
+viewHeader : Html msg
+viewHeader =
+    nav [ class "navbar navbar-light" ]
+        [ div [ class "container" ]
+            [ a [ class "navbar-brand" ]
+                [ text "Ttecho's blog" ]
+            , ul [ class "navbar-nav pull-xs-right" ] <| viewMenu
+            ]
+        ]
+
+
+viewMenu : List (Html msg)
+viewMenu =
+    [ div [ class "navbar-nav nav-item" ] [ text "about" ]
+    , div [ class "navbar-nav nav-item" ] [ text "writings" ]
+    , div [ class "navbar-nav nav-item" ] [ text "music" ]
+    , div [ class "navbar-nav nav-item" ] [ text "code" ]
+    ]
